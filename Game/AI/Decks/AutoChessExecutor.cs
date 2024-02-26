0cd4fdbd9749edf0aa3613c6581fbc2a619d8672
jwyxym <130654339+jwyxym@users.noreply.github.com>
2024-02-26 15:07:57 +0800

    Update AutoChessExecutor.cs

--- Game/AI/Decks/AutoChessExecutor.cs
+++ Game/AI/Decks/AutoChessExecutor.cs
@@ -108,7 +108,84 @@ namespace WindBot.Game.AI.Decks
         {
             HintMsg.SpSummon, HintMsg.ToGrave, HintMsg.AddToHand, HintMsg.FusionMaterial, HintMsg.Destroy
         };
+        
+        //非常愚蠢的穷举函数
+        private bool BlackmailAttackerSunmmon(int cardId)//黑手！（其实是碰瓷）
+        {
+            YGOSharp.OCGWrapper.NamedCard card = YGOSharp.OCGWrapper.NamedCard.Get(cardId);
+            int[] cardsname = new[] {34031284, 35494087, 54366836, 94004268, 97403510, 59627393, 93730230, 69058960, 95442074, 24874631};
+            foreach(int cardname in cardsname)
+            {
+                if (cardId == cardname) return true;
+            }
 
+            if ((card.HasSetcode(0x10) && GetZoneCards(CardLocation.MonsterZone, Bot).Any(scard => scard != null && scard.Id == 29552709 && scard.IsFaceup() && !scard.IsDisabled()) && Duel.Player == 0) || card.HasSetcode(0x2a) || card.HasSetcode(0x1a5) || card.HasSetcode(0x18d) || card.HasSetcode(0x4a))
+                return true;
+
+            return false;
+        }
+
+        private bool BlackmailAttacker(ClientCard card, ClientField player)//让你知道什么是黑手！
+        {
+            if (!card.IsDisabled())
+            {
+                if (card.HasSetcode(0x4a) || card.Id == 34031284 || card.Id == 35494087 || card.Id == 54366836 || card.Id == 94004268 || card.Id == 97403510
+                    || (card.HasSetcode(0x18d) && card.EquipCards.Count()>0)
+                    || (card.Id == 59627393 && card.Overlays.Count()>0)
+                    || (card.Id == 93730230 && card.Overlays.Count()>0)
+                    || (card.Id == 69058960 && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 95442074 && scard.IsFaceup()))
+                    || (card.Id == 95442074 && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 69058960 && scard.IsFaceup()))
+                )
+                    return true;
+            }
+
+            if (card.IsFaceup())
+            {
+                if ((card.HasSetcode(0x1a5) && GetZoneCards(CardLocation.SpellZone, player).Any(scard => scard != null && scard.Id == 65261141 && scard.IsFaceup() && !scard.IsDisabled()))
+                    || (card.HasSetcode(0x2a) && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 17285476 && scard.IsFaceup() && !scard.IsDisabled()))
+                    || (card.Id == 24874631 && GetZoneCards(CardLocation.SpellZone, player).Any(scard => scard != null && scard.Id == 24874630 && scard.IsFaceup() && !scard.IsDisabled()))
+                    || (card.HasSetcode(0x10) && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 29552709 && scard.IsFaceup() && !scard.IsDisabled()))
+                )
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool DontSummon(int cardId)//别通召这些
+        {
+            YGOSharp.OCGWrapper.NamedCard card = YGOSharp.OCGWrapper.NamedCard.Get(cardId);
+            if (card.HasSetcode(0x40) || card.HasSetcode(0xa4) || card.HasSetcode(0xd3)) return true;
+            int[] cardsname = new[] {74762582, 90179822, 16759958, 26964762, 42352091, 2511, 74018812, 76214441, 62886670, 69105797, 32391566, 94076521, 73625877, 1980574, 42090294, 68823957, 34976176, 89785779, 76133574, 3248469
+            , 57647597, 37961969, 51993760, 87988305, 38339996, 37629703, 58131925, 71133680, 42790071, 34475451, 63009228, 24725825, 48427163, 86028783, 51852507, 29280589, 87462901, 73640163, 68120130, 84813516, 55461064, 59042331, 26775203
+            , 67750322, 68819554, 26084285, 15613529, 19096726, 59546797, 12235475, 38695361, 37742478, 26914168, 43534808, 13313278, 99581584, 04192696, 89662736, 81109178, 18444902, 04807253, 12423762, 72318602, 86613346, 82489470, 16223761, 08152834/*时尚小垃圾*/
+            , 97268402/*效果遮蒙者*/, 24508238/*D.D.乌鸦*/, 94145021/*锁鸟*/
+            , 14558127, 14558128, 52038441, 52038442, 59438930, 59438931, 60643553, 60643554, 62015408, 62015409, 73642296, 73642297/*手坑六姐妹*/
+            , 15721123, 23434538, 25137581, 46502744, 80978111, 87170768, 94081496/*xx的G*/
+            , 17266660, 21074344, 94689635/*byd原来宣告者没字段啊*/
+            , 20450925, 19665973, 28427869, 27352108/*攻宣坑*/
+            };
+            foreach(int cardname in cardsname)
+            {
+                if (cardId == cardname) return true;
+            }
+
+            return false;
+        }
+
+        //从神数不神那借来的非常有亲切感的函数 Duel.GetMatchingGroup（不是）
+        private List<ClientCard> GetZoneCards(CardLocation loc, ClientField player)
+        {
+            List<ClientCard> res = new List<ClientCard>();
+            List<ClientCard> temp = new List<ClientCard>();
+            if ((loc & CardLocation.Hand) > 0) { temp = player.Hand.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
+            if ((loc & CardLocation.MonsterZone) > 0) { temp = player.GetMonsters(); if (temp.Count() > 0) res.AddRange(temp); }
+            if ((loc & CardLocation.SpellZone) > 0) { temp = player.GetSpells(); if (temp.Count() > 0) res.AddRange(temp); }
+            if ((loc & CardLocation.Grave) > 0) { temp = player.Graveyard.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
+            if ((loc & CardLocation.Removed) > 0) { temp = player.Banished.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
+            if ((loc & CardLocation.Extra) > 0) { temp = player.ExtraDeck.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
+            return res;
+        }
         public override IList<ClientCard> OnSelectCard(IList<ClientCard> _cards, int min, int max, int hint, bool cancelable)
         {
             if (Duel.Phase == DuelPhase.BattleStart)
@@ -231,6 +308,22 @@ namespace WindBot.Game.AI.Decks
 
                 return false;
             }
+            else if (Card.Id == 84815190)
+            {
+                if (ActivateDescription == Util.GetStringId(84815190, 0))
+                {
+                    List<ClientCard> cards = GetZoneCards(CardLocation.Onfield, Enemy);
+                    cards = cards.Where(tcard => tcard != null && !tcard.IsShouldNotBeTarget()).ToList();
+                    if (cards.Count <= 0) return false;
+                    AI.SelectCard(cards);
+                    return true;
+                }
+                else if (ActivateDescription == Util.GetStringId(84815190, 1))
+                {
+                    return Duel.LastChainPlayer == 1;
+                }
+                return false;
+            }
             return DefaultDontChainMyself();
         }
 
@@ -259,13 +352,9 @@ namespace WindBot.Game.AI.Decks
             if (!attacker.IsAttack())
                 atk = attacker.GetDefensePower();
 
-            List<ClientCard> cards = defenders.Where(defender => defender != null && !BlackmailAttacker(defender, Enemy) && defender.IsFaceup()).ToList();
-            List<ClientCard> cards1 = defenders.Where(defender => defender != null && !BlackmailAttacker(defender, Enemy) && defender.IsFaceup() && ((defender.IsAttack() && defender.Attack >= atk) || (!defender.IsAttack() && defender.GetDefensePower() >= atk))).ToList();
+            List<ClientCard> cards1 = defenders.Where(defender => defender != null && !BlackmailAttacker(defender, Enemy) && defender.IsFaceup() && defender.GetDefensePower() >= atk).ToList();
             List<ClientCard> cards2 = defenders.Where(defender => defender != null && !BlackmailAttacker(defender, Enemy) && defender.IsFaceup() && defender.IsAttack() && defender.Attack < atk).ToList();
             cards2.Sort(CardContainer.CompareCardAttack);
-            List<ClientCard> cards3 = defenders.Where(defender => defender != null && !BlackmailAttacker(defender, Enemy) && defender.IsFaceup() && !defender.IsAttack() && defender.GetDefensePower() < atk).ToList();
-            List<ClientCard> cards4 = defenders.Where(defender => defender != null && defender.IsFacedown()).ToList();
-            List<ClientCard> cards5 = defenders.Where(defender => defender != null && BlackmailAttacker(defender, Enemy) && defender.IsFaceup()).ToList();
 
             if (BlackmailAttacker(attacker, Bot))
             {
@@ -292,101 +381,30 @@ namespace WindBot.Game.AI.Decks
                 {
                     return AI.Attack(attacker, cards2[0]);
                 }
-                else if (cards4.Count() > 0)
-                    return AI.Attack(attacker, cards4[0]);
-                else if (cards3.Count() > 0)
-                    return AI.Attack(attacker, cards3[0]);
-                else if (cards5.Count() > 0)
-                    return base.OnSelectAttackTarget(attacker, cards5);
+                    return AI.Attack(attacker, defenders[Program.Rand.Next(defenders.Count())]);
             }
 
-            if (cards5.Count() > 0 && cards5.Any(defender => defender != null && defender.HasSetcode(0x10)))
+            if (defenders.Any(defender => defender != null && defender.HasSetcode(0x10) && BlackmailAttacker(defender, Enemy)))
                 {
                     List<ClientCard> scards = defenders.Where(defender => defender != null && defender.Id == 69058960 && !defender.IsDisabled() && defender.IsFaceup()).ToList();
                     return base.OnSelectAttackTarget(attacker, scards);
                 }
-            else if (cards2.Count() > 0)
-                return base.OnSelectAttackTarget(attacker, cards2);
-            else if (cards4.Count() > 0 && (attacker.Attack >= 1500 || Bot.LifePoints >= 1000))
-                return AI.Attack(attacker, cards4[0]);
-            else if (cards3.Count() > 0)
-                return base.OnSelectAttackTarget(attacker, cards3);
+
+            foreach (ClientCard defender in defenders)
+            {
+                if (!base.OnPreBattleBetween(attacker, defender) || BlackmailAttacker(defender, Enemy))
+                    continue;
+
+                if ((atk > defender.GetDefensePower()) || (atk >= defender.GetDefensePower() && attacker.IsLastAttacker && defender.IsAttack()) || (defender.IsFacedown()))
+                    return AI.Attack(attacker, defender);
+            }
+
             return null;
         }
 
-        private bool BlackmailAttacker(ClientCard card, ClientField player)
-        {
-            if (!card.IsDisabled())
-            {
-                if (card.HasSetcode(0x4a) || card.Id == 34031284 || card.Id == 35494087 || card.Id == 54366836 || card.Id == 94004268 || card.Id == 97403510
-                    || (card.HasSetcode(0x18d) && card.EquipCards.Count()>0)
-                    || (card.Id == 59627393 && card.Overlays.Count()>0)
-                    || (card.Id == 93730230 && card.Overlays.Count()>0)
-                    || (card.Id == 69058960 && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 95442074 && scard.IsFaceup()))
-                    || (card.Id == 95442074 && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 69058960 && scard.IsFaceup()))
-                )
-                    return true;
-            }
 
-            if (card.IsFaceup())
-            {
-                if ((card.HasSetcode(0x1a5) && GetZoneCards(CardLocation.SpellZone, player).Any(scard => scard != null && scard.Id == 65261141 && scard.IsFaceup() && !scard.IsDisabled()))
-                    || (card.HasSetcode(0x2a) && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 17285476 && scard.IsFaceup() && !scard.IsDisabled()))
-                    || (card.Id == 24874631 && GetZoneCards(CardLocation.SpellZone, player).Any(scard => scard != null && scard.Id == 24874630 && scard.IsFaceup() && !scard.IsDisabled()))
-                    || (card.HasSetcode(0x10) && GetZoneCards(CardLocation.MonsterZone, player).Any(scard => scard != null && scard.Id == 29552709 && scard.IsFaceup() && !scard.IsDisabled()))
-                )
-                    return true;
-            }
 
-            return false;
-        }
 
-        private bool BlackmailAttackerSunmmon(int cardId)
-        {
-            YGOSharp.OCGWrapper.NamedCard card = YGOSharp.OCGWrapper.NamedCard.Get(cardId);
-            int[] cardsname = new[] {34031284, 35494087, 54366836, 94004268, 97403510, 59627393, 93730230, 69058960, 95442074, 24874631};
-            foreach(int cardname in cardsname)
-            {
-                if (cardId == cardname) return true;
-            }
-
-            if ((card.HasSetcode(0x10) && GetZoneCards(CardLocation.MonsterZone, Bot).Any(scard => scard != null && scard.Id == 29552709 && scard.IsFaceup() && !scard.IsDisabled()) && Duel.Player == 0) || card.HasSetcode(0x2a) || card.HasSetcode(0x1a5) || card.HasSetcode(0x18d) || card.HasSetcode(0x4a))
-                return true;
-
-            return false;
-        }
-        private bool DontSummon(int cardId)
-        {
-            YGOSharp.OCGWrapper.NamedCard card = YGOSharp.OCGWrapper.NamedCard.Get(cardId);
-            if (card.HasSetcode(0x40) || card.HasSetcode(0xa4)) return true;
-            int[] cardsname = new[] {74762582, 90179822, 16759958, 26964762, 42352091, 2511, 74018812, 76214441, 62886670, 69105797, 32391566, 94076521, 73625877, 1980574, 42090294, 68823957, 34976176, 89785779, 76133574, 3248469
-            , 57647597, 37961969, 51993760, 87988305, 38339996, 37629703, 58131925, 71133680, 42790071, 34475451, 63009228, 24725825, 48427163, 86028783, 51852507, 29280589, 87462901, 73640163, 68120130, 84813516, 55461064, 59042331, 26775203
-            , 67750322, 68819554, 26084285, 15613529, 19096726, 59546797, 12235475, 38695361, 37742478, 26914168, 43534808, 13313278, 99581584, 04192696, 89662736, 81109178, 18444902, 04807253, 12423762, 72318602, 86613346, 82489470, 16223761, 08152834/*时尚小垃圾*/
-            , 97268402/*效果遮蒙者*/, 24508238/*D.D.乌鸦*/, 94145021/*锁鸟*/
-            , 14558127, 14558128, 52038441, 52038442, 59438930, 59438931, 60643553, 60643554, 62015408, 62015409, 73642296, 73642297/*手坑六姐妹*/
-            , 15721123, 23434538, 25137581, 46502744, 80978111, 87170768, 94081496/*xx的G*/
-            , 17266660, 21074344, 94689635/*byd原来宣告者没字段啊*/
-            , 20450925, 19665973, 28427869, 27352108/*攻宣坑*/
-            };
-            foreach(int cardname in cardsname)
-            {
-                if (cardId == cardname) return true;
-            }
-
-            return false;
-        }
-        private List<ClientCard> GetZoneCards(CardLocation loc, ClientField player)
-        {
-            List<ClientCard> res = new List<ClientCard>();
-            List<ClientCard> temp = new List<ClientCard>();
-            if ((loc & CardLocation.Hand) > 0) { temp = player.Hand.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
-            if ((loc & CardLocation.MonsterZone) > 0) { temp = player.GetMonsters(); if (temp.Count() > 0) res.AddRange(temp); }
-            if ((loc & CardLocation.SpellZone) > 0) { temp = player.GetSpells(); if (temp.Count() > 0) res.AddRange(temp); }
-            if ((loc & CardLocation.Grave) > 0) { temp = player.Graveyard.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
-            if ((loc & CardLocation.Removed) > 0) { temp = player.Banished.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
-            if ((loc & CardLocation.Extra) > 0) { temp = player.ExtraDeck.Where(card => card != null).ToList(); if (temp.Count() > 0) res.AddRange(temp); }
-            return res;
-        }
         private bool MonsterRepos()
         {
             if (Duel.Phase == DuelPhase.Main1 && Card.HasType(CardType.Flip) && Card.IsFacedown())
